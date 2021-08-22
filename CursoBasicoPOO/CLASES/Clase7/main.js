2ff49fd68d88416328d4c763defc29081035c5f7
Carlos Iván Ochoa Gómez <81680537+CIOG2@users.noreply.github.com>
2021-08-22 02:04:36 -0600

    Objetos literles

---
+++ CursoBasicoPOO/CLASES/Clase7/main.js
@@ -0,0 +1,67 @@
+const Ivan1 = {
+    name: 'Ivan',
+    userName: 'CIOG',
+    points: 7946,
+    socialMedia: {
+        twitter: 'Carlos_GO',
+        instagram: 'CIOG2',
+        facebook: undefined,
+    },
+    approvedCourses: [
+        'Curso Definitivo de HTML y CSS',
+        'Curso practico de HTML y CSS',
+        'Curso Profecional de Javascript'
+    ],
+    learnignpaths: [
+        {
+            name: 'Escuela de desarrollo web',
+            courses: [
+                'Curso Definitivo de HTML y CSS',
+                'Curso practico de HTML y CSS',
+                'Curso Profecional de Javascript'        
+            ],
+        },
+        {
+            name: 'Escuela de Javascript',
+            courses: [
+                'Curso de Front-end developer',
+                'Curso Asincronismo',
+                'Curso Emascript6'        
+            ],
+        },
+    ]
+}
+
+const zeus = {
+    name: 'Zeus',
+    userName: 'ZeusGamer',
+    points: 1306,
+    socialMedia: {
+        twitter: 'ZEUS_GO',
+        instagram: 'zeus2',
+        facebook: undefined,
+    },
+    approvedCourses: [
+        'Curso Definitivo de HTML y CSS',
+        'Curso practico de HTML y CSS',
+        'Curso Profecional de Javascript'
+    ],
+    learnignpaths: [
+        {
+            name: 'Escuela de desarrollo web',
+            courses: [
+                'Curso Definitivo de HTML y CSS',
+                'Curso practico de HTML y CSS',
+                'Curso Profecional de Javascript'        
+            ],
+        },
+        {
+            name: 'Escuela de Video Juegos',
+            courses: [
+                'Curso de introduccion a la produccion de vgs',
+                'Curso de unReal Engine',
+                'Curso de Unity 3D',
+            ],
+        },
+    ]
+}
